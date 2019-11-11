69f2c6b255c352cf6365e56a053fcc902014a529
xiezhendong <xiezhendong@i-games.com.cn>
2019-11-11 20:39:45 +0800

    ZED::简化框架目录结构。

---
+++ Assets/TinFramework/Scripts/TimeModule/TimeModule.cs
@@ -0,0 +1,25 @@
+/****************************************************
+文件：TimeModule.cs
+作者：ADMIN
+日期：2019/11/11 20:38:08
+功能：Noting
+*****************************************************/
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class TimeModule : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
+
