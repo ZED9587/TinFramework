e3929bfd682d92fac13aa01fb2346cab8a6b24c2
xiezhendong <xiezhendong@i-games.com.cn>
2019-11-11 21:19:14 +0800

    ZED::增加ScriptableObject自动生成工具。

--- Assets/TinFramework/Editor/GenerateSOFile.cs
+++ Assets/TinFramework/Editor/GenerateSOFile.cs
@@ -18,37 +18,20 @@ public class GenerateSOFile : Editor
         Object so = Selection.activeObject;
         if (null != so)
         {
-            Debug.Log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx--->" + so.name);
             string filePath = AssetDatabase.GetAssetPath(so);
-            Debug.Log("xxxxxxxxxxxxxxxxxxxxxxxxxxxxxx--->" + filePath);
             if (filePath.EndsWith(".cs"))
             {
                 ScriptableObject soFile = ScriptableObject.CreateInstance(so.name);
                 if (null != soFile)
                 {
+                    AssetDatabase.CreateAsset(soFile, filePath.Replace(".cs", ".asset"));
                     AssetDatabase.SaveAssets();
+                    AssetDatabase.Refresh();
                 }
-                AssetDatabase.Refresh();
-                //if (typeof(ScriptableObject).IsAssignableFrom(so.GetType()))
-                //{
-                //    ScriptableObject soFile = ScriptableObject.CreateInstance(so.name);
-                //    if (null != soFile)
-                //    {
-                //        AssetDatabase.SaveAssets();
-                //    }
-                //    AssetDatabase.Refresh();
-                //}
-                //else
-                //{
-                //    Debug.Log("HELLLLLLLLLLLLLLLLLLLL");
-                //}
-                //TextAsset asset = AssetDatabase.LoadAssetAtPath<TextAsset>(filePath);
-                //if (null != asset)
-                //{
-                //    Assembly assembly = Assembly.Load(asset.bytes);
-                //    var type =  assembly.GetType(so.name);
-
-                //}
+            }
+            else
+            {
+                Debug.LogError("The File Must Be A CS FILE.");
             }
         }
     }
