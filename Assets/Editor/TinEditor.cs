19faf6e87e154cfcf7c9329fcb39cde8fcb655f3
xiezhendong <2511607672@qq.com>
2020-02-28 11:43:00 +0800

    add::增加编辑器下的全局配置界面。

---
+++ Assets/Editor/TinEditor.cs
@@ -0,0 +1,14 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TinEditor : Editor
+{
+    [MenuItem("游戏设计工具/全局配置界面")]
+    public static void Tin() {
+        //open editorwindow
+        EditorWindow editorWindow = EditorWindow.GetWindow<TinEditorWindow>();
+        editorWindow.Show();
+    }
+}
---
+++ Assets/Editor/TinEditorWindow.cs
@@ -0,0 +1,16 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEditor;
+
+public class TinEditorWindow : EditorWindow
+{
+    public TinEditorWindow() {
+        this.titleContent = new GUIContent("全局配置界面");
+    }
+
+    public void OnGUI()
+    {
+
+    }
+}
